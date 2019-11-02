b6d42e17ffdbb3895966d20cb42ccbcc6020e404
EmilieLamps <emilie.lamps@gmail.com>
2019-11-02 10:55:05 +0100

    Exercice corrigé + css

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Déclaration des variables
+let display = document.getElementById('display');
+let hide = document.getElementById('hide');
+let text = document.getElementById('text');
+
+// Application des événements + Appel des fonctions
+display.onclick = function() {
+  text.style.visibility = 'visible';
+};
+hide.onclick = function() {
+  text.style.visibility = 'hidden';
+};
